438729b6c07b70a9d0816a4827a4893268c702c9
Tucker Osman <osmiumusa@gmail.com>
2017-08-12 23:51:38 -0500

    Initial commit with basic API

---
+++ transponder.js
@@ -0,0 +1,20 @@
+const io = require("socket.io-client");
+const socket = io.connect("http://localhost:41838");
+const name = "echotest"
+
+socket.on("connect",function() {
+    socket.emit("declare", {name});
+});
+
+socket.on("ready",function() {
+    console.log("I was accepted!");
+});
+
+socket.on("not_allowed", function() {
+    console.log("I was not allowed");
+});
+
+socket.on("message", function(words) {
+    console.log(words);
+    socket.emit("message", words.join(" "));
+});
